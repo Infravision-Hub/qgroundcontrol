dc3f7773d9f00062d563838650d64bb13b9aa1b8
Troy <troy.doropoulos@infravision.com.au>
2026-03-27 13:21:44 +1100

    QGC Zombie Watchdog Patch / Flopper Engage

--- src/Vehicle/MultiVehicleManager.cc
+++ src/Vehicle/MultiVehicleManager.cc
@@ -86,6 +86,14 @@ void MultiVehicleManager::init()
     (void) connect(_gcsHeartbeatTimer, &QTimer::timeout, this, &MultiVehicleManager::_sendGCSHeartbeat);
     _gcsHeartbeatTimer->start();
 
+    // --- PATCH: Step D4 - Start the RC Override Keepalive timer ---
+    _gcsRcKeepaliveTimer = new QTimer(this);
+    _gcsRcKeepaliveTimer->setInterval(kGCSRcKeepaliveRateMSecs);
+    _gcsRcKeepaliveTimer->setSingleShot(false);
+    connect(_gcsRcKeepaliveTimer, &QTimer::timeout, this, &MultiVehicleManager::_sendGCSRcOverrideKeepalive);
+    _gcsRcKeepaliveTimer->start();
+    // --------------------------------------------------------------
+
     _initialized = true;
 }
 
@@ -347,6 +355,59 @@ void MultiVehicleManager::_sendGCSHeartbeat()
         (void) link->writeBytesThreadSafe(reinterpret_cast<const char*>(buffer), len);
     }
 }
+
+// --- PATCH: Step D5 - Implement _sendGCSRcOverrideKeepalive() ---
+void MultiVehicleManager::_sendGCSRcOverrideKeepalive()
+{
+    if (!SettingsManager::instance()->mavlinkSettings()->sendGCSHeartbeat()->rawValue().toBool()) {
+        return;
+    }
+
+    Vehicle* v = activeVehicle();
+    if (!v) {
+        return;
+    }
+
+    // flip-flop pwm
+    const uint16_t pwm = _rcKeepaliveFlip ? 1900 : 1100;
+    _rcKeepaliveFlip = !_rcKeepaliveFlip;
+
+    constexpr uint16_t IGNORE = UINT16_MAX; // 65535 (ignore this channel) - ArduPilot supports this semantics
+
+    const auto links = LinkManager::instance()->links();
+    for (const SharedLinkInterfacePtr& link : links) {
+        if (!link->isConnected()) {
+            continue;
+        }
+
+        const auto linkConfig = link->linkConfiguration();
+        if (linkConfig && linkConfig->isHighLatency()) {
+            continue;
+        }
+
+        mavlink_message_t msg{};
+
+        // IMPORTANT: only chan16 gets a value; everything else is ignored.
+        mavlink_msg_rc_channels_override_pack_chan(
+            MAVLinkProtocol::instance()->getSystemId(),
+            MAVLinkProtocol::instance()->getComponentId(),
+            link->mavlinkChannel(),
+            &msg,
+            static_cast<uint8_t>(v->id()),  // target_system (vehicle sysid)
+            0,                                // target_component
+            IGNORE, IGNORE, IGNORE, IGNORE, IGNORE, IGNORE, IGNORE, IGNORE,
+            IGNORE, IGNORE, IGNORE, IGNORE, IGNORE, IGNORE, IGNORE,
+            pwm,                               // chan16_raw
+            IGNORE, IGNORE
+        );
+
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN]{};
+        const uint16_t len = mavlink_msg_to_send_buffer(buffer, &msg);
+
+        link->writeBytesThreadSafe(reinterpret_cast<const char*>(buffer), len);
+    }
+}
+// ----------------------------------------------------------------
 
 void MultiVehicleManager::selectVehicle(int vehicleId)
 {
--- src/Vehicle/MultiVehicleManager.h
+++ src/Vehicle/MultiVehicleManager.h
@@ -7,7 +7,6 @@
  *
  ****************************************************************************/
 
-
 /// @file
 ///     @author Don Gagne <don@thegagnes.com>
 
@@ -31,10 +30,10 @@ class MultiVehicleManager : public QObject
     Q_MOC_INCLUDE("Vehicle.h")
     Q_PROPERTY(bool                 activeVehicleAvailable          READ _getActiveVehicleAvailable                                         NOTIFY activeVehicleAvailableChanged)
     Q_PROPERTY(bool                 parameterReadyVehicleAvailable  READ _getParameterReadyVehicleAvailable                                 NOTIFY parameterReadyVehicleAvailableChanged)
-    Q_PROPERTY(Vehicle              *activeVehicle                  READ activeVehicle                      WRITE setActiveVehicle          NOTIFY activeVehicleChanged)
-    Q_PROPERTY(QmlObjectListModel   *vehicles                       READ vehicles                                                           CONSTANT)
-    Q_PROPERTY(QmlObjectListModel   *selectedVehicles               READ selectedVehicles                                                   CONSTANT)
-    Q_PROPERTY(Vehicle              *offlineEditingVehicle          READ offlineEditingVehicle                                              CONSTANT)
+    Q_PROPERTY(Vehicle              *activeVehicle                  READ activeVehicle                  WRITE setActiveVehicle          NOTIFY activeVehicleChanged)
+    Q_PROPERTY(QmlObjectListModel   *vehicles                       READ vehicles                                                                   CONSTANT)
+    Q_PROPERTY(QmlObjectListModel   *selectedVehicles               READ selectedVehicles                                                           CONSTANT)
+    Q_PROPERTY(Vehicle              *offlineEditingVehicle          READ offlineEditingVehicle                                                      CONSTANT)
 
 public:
     explicit MultiVehicleManager(QObject *parent = nullptr);
@@ -46,8 +45,8 @@ public:
     void init();
     Q_INVOKABLE Vehicle *getVehicleById(int vehicleId) const;
     Q_INVOKABLE void      selectVehicle(int vehicleId);
-    Q_INVOKABLE void    deselectVehicle(int vehicleId);
-    Q_INVOKABLE void    deselectAllVehicles();
+    Q_INVOKABLE void     deselectVehicle(int vehicleId);
+    Q_INVOKABLE void     deselectAllVehicles();
     QmlObjectListModel *vehicles() const { return _vehicles; }
     QmlObjectListModel *selectedVehicles() const { return _selectedVehicles; }
     Vehicle *offlineEditingVehicle() const { return _offlineEditingVehicle; }
@@ -67,6 +66,7 @@ private slots:
     void _setActiveVehiclePhase2(Vehicle *vehicle);
     void _vehicleParametersReadyChanged(bool parametersReady);
     void _sendGCSHeartbeat();
+    void _sendGCSRcOverrideKeepalive(); // Added for keepalive patch
     void _vehicleHeartbeatInfo(LinkInterface *link, int vehicleId, int componentId, int vehicleFirmwareType, int vehicleType);
     void _requestProtocolVersion(unsigned version) const; /// This slot is connected to the Vehicle::requestProtocolVersion signal such that the vehicle manager tries to switch MAVLink to v2 if all vehicles support it
 
@@ -80,14 +80,17 @@ private:
     void _setParameterReadyVehicleAvailable(bool parametersReady);
 
     QTimer *_gcsHeartbeatTimer = nullptr;           ///< Timer to emit heartbeats
+    QTimer *_gcsRcKeepaliveTimer = nullptr;         ///< Added: Timer for keepalive logic
     QmlObjectListModel *_vehicles = nullptr;
     QmlObjectListModel *_selectedVehicles = nullptr;
     Vehicle *_offlineEditingVehicle = nullptr;      ///< Disconnected vechicle used for offline editing
     bool _activeVehicleAvailable = false;           ///< true: An active vehicle is available
     bool _parameterReadyVehicleAvailable = false;   ///< true: An active vehicle with ready parameters is available
+    bool _rcKeepaliveFlip = false;                  ///< Added: State for keepalive toggle
     Vehicle *_activeVehicle = nullptr;              ///< Currently active vehicle from a ui perspective
     QList<int> _ignoreVehicleIds;                   ///< List of vehicle id for which we ignore further communication
     bool _initialized = false;
 
     static constexpr int kGCSHeartbeatRateMSecs = 1000;  ///< Heartbeat rate
+    static constexpr int kGCSRcKeepaliveRateMSecs = 500; ///< Added: 500ms Keepalive rate
 };
